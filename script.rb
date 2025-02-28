e78ae5929f648613af8d48f5801b8c0acc6ce637
nickfrn <193815257+nickfrn@users.noreply.github.com>
2025-02-28 17:56:39 +0100

    Add script.rb with full working logic

---
+++ script.rb
@@ -0,0 +1,23 @@
+def substrings(string, dict)
+  matches = Hash.new
+
+  string_array = string.split(" ")
+
+  string_array.each do |word|
+    dict.each do |dict_word|
+      if word.include?(dict_word)
+        unless matches[dict_word]
+          matches[dict_word] = 1
+        else
+          matches[dict_word] += 1
+        end
+      end
+    end
+  end
+
+  matches
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
